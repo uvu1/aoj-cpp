45a481561419db50498293c5ef4d632d37566fb7
uvu1 <53385458+uvu1@users.noreply.github.com>
2025-08-07 08:39:59 +0900

    solve problem

--- range/a.cpp
+++ range/a.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 int main() {
-    int N;
+    int a, b, c;
+    cin >> a >> b >> c;
+    if(a<b and b<c) cout << "Yes" << endl; // a < b < cは(a<b)<cというようにコンパイラに解釈されるため意味を持たない
+    else cout << "No" << endl;
     return 0;
 }
